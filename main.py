dab160bcaf4793cfc98515ca018b67836780e44d
Khushboo <khushboojain34@gmail.com>
2020-04-23 19:23:52 +0530

    Initialized Main function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello Jinank")
+def get_integer(): 
+	result = int(input("Enter integer: ")) 
+	return result 
+
+def main():   
+	output = get_integer()	 
+	print(f"Hello World {output}") 
+
+
+if __name__ == "__main__": 
+	main() 
